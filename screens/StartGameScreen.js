f9e7dfd5d4aa38c9d55ca273d93a6e2ac13594b8
Jeremy Twidt <jtwidt@gmail.com>
2024-09-17 14:37:50 -0500

    screen file creation

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInput, View } from 'react-native';
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+      {/* BUTTONS */}
+    </View>
+  );
+};
+
+export default StartGameScreen;
