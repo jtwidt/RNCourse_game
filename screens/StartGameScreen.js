e929c73d62667cffa2aa7f899f8d7a570031f50b
Jeremy Twidt <jtwidt@gmail.com>
2024-09-18 16:08:58 -0500

    styling changes

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -12,18 +12,26 @@ const StartGameScreen = () => {
         autoCapitalize='none'
         autoCorrect={false}
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 16,
     marginTop: 100,
     marginHorizontal: 24,
-    backgroundColor: '#4e0329',
+    backgroundColor: '#3b012f',
     borderRadius: 8,
     elevation: 4, // elevation is Android only
     shadowColor: 'black', // shadow_ properties are iOS only
@@ -41,6 +49,12 @@ const styles = StyleSheet.create({
     color: '#ddb52f',
     marginVertical: 8,
     textAlign: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
 
